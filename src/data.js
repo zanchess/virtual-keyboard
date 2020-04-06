0e92e79a599dbb98e94240fe5a87e5930ebacc04
Lashkevich Alexandr <zanchesssss@gmail.com>
2020-04-06 23:32:39 +0300

    feat: add data keyboard

---
+++ src/data.js
@@ -0,0 +1,205 @@
+export const notSymbol = ['Backspace', 'Tab', 'Delete', 'CapsLock', 'Enter', 'ShiftLeft', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'AltRight', 'ControlRight'];
+export const dataKeyLetters = [
+  {
+    keyCode: 192, code: 'Backquote', RU: 'ё', EN: '`', key: 'tilde', symbol: '~',
+  },
+  {
+    keyCode: 81, code: 'KeyQ', RU: 'Й', EN: 'Q', key: 'letter_Q', symbol: null,
+  },
+  {
+    keyCode: 87, code: 'KeyW', RU: 'Ц', EN: 'W', key: 'letter_W', symbol: null,
+  },
+  {
+    keyCode: 69, code: 'KeyE', RU: 'У', EN: 'E', key: 'letter_E', symbol: null,
+  },
+  {
+    keyCode: 82, code: 'KeyR', RU: 'К', EN: 'R', key: 'letter_R', symbol: null,
+  },
+  {
+    keyCode: 84, code: 'KeyT', RU: 'Е', EN: 'T', key: 'letter_T', symbol: null,
+  },
+  {
+    keyCode: 89, code: 'KeyY', RU: 'Н', EN: 'Y', key: 'letter_Y', symbol: null,
+  },
+  {
+    keyCode: 85, code: 'KeyU', RU: 'Г', EN: 'U', key: 'letter_U', symbol: null,
+  },
+  {
+    keyCode: 73, code: 'KeyI', RU: 'Ш', EN: 'I', key: 'letter_I', symbol: null,
+  },
+  {
+    keyCode: 79, code: 'KeyO', RU: 'Щ', EN: 'O', key: 'letter_O', symbol: null,
+  },
+  {
+    keyCode: 80, code: 'KeyP', RU: 'З', EN: 'P', key: 'letter_P', symbol: null,
+  },
+  {
+    keyCode: 219, code: 'BracketLeft', RU: 'Х', EN: '[', key: 'bracket__sq_l', symbol: null,
+  },
+  {
+    keyCode: 221, code: 'BracketRight', RU: 'Ъ', EN: ']', key: 'bracket__sq_r', symbol: null,
+  },
+  {
+    keyCode: 220, code: 'Backslash', RU: '\\', EN: '\\', key: 'trait', symbol: '/',
+  },
+  {
+    keyCode: 65, code: 'KeyA', RU: 'Ф', EN: 'A', key: 'letter_A', symbol: null,
+  },
+  {
+    keyCode: 83, code: 'KeyS', RU: 'Ы', EN: 'S', key: 'letter_S', symbol: null,
+  },
+  {
+    keyCode: 68, code: 'KeyD', RU: 'В', EN: 'D', key: 'letter_D', symbol: null,
+  },
+  {
+    keyCode: 70, code: 'KeyF', RU: 'А', EN: 'F', key: 'letter_F', symbol: null,
+  },
+  {
+    keyCode: 71, code: 'KeyG', RU: 'П', EN: 'G', key: 'letter_G', symbol: null,
+  },
+  {
+    keyCode: 72, code: 'KeyH', RU: 'Р', EN: 'H', key: 'letter_H', symbol: null,
+  },
+  {
+    keyCode: 74, code: 'KeyJ', RU: 'О', EN: 'J', key: 'letter_J', symbol: null,
+  },
+  {
+    keyCode: 75, code: 'KeyK', RU: 'Л', EN: 'K', key: 'letter_K', symbol: null,
+  },
+  {
+    keyCode: 76, code: 'KeyL', RU: 'Д', EN: 'L', key: 'letter_L', symbol: null,
+  },
+  {
+    keyCode: 186, code: 'Semicolon', RU: 'Ж', EN: ';', key: 'semicolon', symbol: null,
+  },
+  {
+    keyCode: 222, code: 'Quote', RU: 'Э', EN: "'", key: 'mark', symbol: null,
+  },
+  {
+    keyCode: 226, code: 'IntlBackslash', RU: '&#92;', EN: '&#92;', key: 'slash_l', symbol: null,
+  },
+  {
+    keyCode: 90, code: 'KeyZ', RU: 'Я', EN: 'Z', key: 'letter_Z', symbol: null,
+  },
+  {
+    keyCode: 88, code: 'KeyX', RU: 'Ч', EN: 'X', key: 'letter_X', symbol: null,
+  },
+  {
+    keyCode: 67, code: 'KeyC', RU: 'С', EN: 'C', key: 'letter_C', symbol: null,
+  },
+  {
+    keyCode: 86, code: 'KeyV', RU: 'М', EN: 'V', key: 'letter_V', symbol: null,
+  },
+  {
+    keyCode: 66, code: 'KeyB', RU: 'И', EN: 'B', key: 'letter_B', symbol: null,
+  },
+  {
+    keyCode: 78, code: 'KeyN', RU: 'Т', EN: 'N', key: 'letter_N', symbol: null,
+  },
+  {
+    keyCode: 77, code: 'KeyM', RU: 'Ь', EN: 'M', key: 'letter_M', symbol: null,
+  },
+  {
+    keyCode: 188, code: 'Comma', RU: 'Б', EN: ',', key: 'comma', symbol: null,
+  },
+  {
+    keyCode: 190, code: 'Period', RU: 'Ю', EN: '.', key: 'point', symbol: null,
+  },
+  {
+    keyCode: 191, code: 'Slash', RU: '/', EN: '/', key: 'slash_r', symbol: null,
+  },
+
+];
+
+export const dataKeyNumbers = [
+  {
+    keyCode: 49, code: 'Digit1', RU: '1', EN: '1', key: 'one', symbol: '!',
+  },
+  {
+    keyCode: 50, code: 'Digit2', RU: '2', EN: '2', key: 'two', symbol: '"',
+  },
+  {
+    keyCode: 51, code: 'Digit3', RU: '3', EN: '3', key: 'three', symbol: '№',
+  },
+  {
+    keyCode: 52, code: 'Digit4', RU: '4', EN: '4', key: 'four', symbol: ';',
+  },
+  {
+    keyCode: 53, code: 'Digit5', RU: '5', EN: '5', key: 'five', symbol: '%',
+  },
+  {
+    keyCode: 54, code: 'Digit6', RU: '6', EN: '6', key: 'six', symbol: '^',
+  },
+  {
+    keyCode: 55, code: 'Digit7', RU: '7', EN: '7', key: 'seven', symbol: '&',
+  },
+  {
+    keyCode: 56, code: 'Digit8', RU: '8', EN: '8', key: 'eight', symbol: '*',
+  },
+  {
+    keyCode: 57, code: 'Digit9', RU: '9', EN: '9', key: 'nine', symbol: '(',
+  },
+  {
+    keyCode: 48, code: 'Digit0', RU: '0', EN: '0', key: 'zero', symbol: ')',
+  },
+  {
+    keyCode: 189, code: 'Minus', RU: '-', EN: '-', key: 'minus', symbol: '_',
+  },
+  {
+    keyCode: 187, code: 'Equal', RU: '=', EN: '=', key: 'plus', symbol: '+',
+  },
+];
+
+export const dataKeyFunc = [
+  {
+    keyCode: 8, code: 'Backspace', RU: 'Backspace', EN: 'Backspace', key: 'back', symbol: null,
+  },
+  {
+    keyCode: 46, code: 'Delete', RU: 'DEL', EN: 'DEL', key: 'del', symbol: null,
+  },
+  {
+    keyCode: 9, code: 'Tab', RU: 'TAB', EN: 'TAB', key: 'tab', symbol: 'tab',
+  },
+  {
+    keyCode: 20, code: 'CapsLock', RU: 'Caps Lock', EN: 'Caps Lock', key: 'caps', symbol: null,
+  },
+  {
+    keyCode: 220, code: 'Enter', RU: 'ENTER', EN: 'ENTER', key: 'enter', symbol: null,
+  },
+  {
+    keyCode: 16, code: 'ShiftLeft', RU: 'SHIFT', EN: 'SHIFT', key: 'shift_1', symbol: null,
+  },
+  {
+    keyCode: 38, code: 'ArrowUp', RU: '&uarr;', EN: '&uarr;', key: 'up', symbol: null,
+  },
+  {
+    keyCode: 16, code: 'ShiftRight', RU: 'SHIFT', EN: 'SHIFT', key: 'shift_2', symbol: null,
+  },
+  {
+    keyCode: 17, code: 'ControlLeft', RU: 'Ctrl', EN: 'Ctrl', key: 'ctrl_1', symbol: null,
+  },
+  {
+    keyCode: 91, code: 'MetaLeft', RU: 'Win', EN: 'Win', key: 'window', symbol: null,
+  },
+  {
+    keyCode: 18, code: 'AltLeft', RU: 'Alt', EN: 'Alt', key: 'alt_1', symbol: null,
+  },
+  {
+    keyCode: 32, code: 'Space', RU: ' ', EN: ' ', key: 'space', symbol: null,
+  },
+  {
+    keyCode: 18, code: 'AltRight', RU: 'Alt', EN: 'Alt', key: 'alt_2', symbol: null,
+  },
+  {
+    keyCode: 17, code: 'ControlRight', RU: 'Ctrl', EN: 'Ctrl', key: 'ctrl_2', symbol: null,
+  },
+  {
+    keyCode: 37, code: 'ArrowLeft', RU: '&larr;', EN: '&larr;', key: 'left', symbol: null,
+  },
+  {
+    keyCode: 40, code: 'ArrowDown', RU: '&darr;', EN: '&darr;', key: 'down', symbol: null,
+  },
+  {
+    keyCode: 39, code: 'ArrowRight', RU: '&rarr;', EN: '&rarr;', key: 'right', symbol: null,
+  },
+];
